31bc9ef61a3ade98de2effe87fe8150e57aa87b1
Cindy Chen <ches53749@gmail.com>
2025-03-06 11:40:21 +0000

    commit

--- collabw7.R
+++ collabw7.R
@@ -1,3 +1,18 @@
+# How does current academic performance predict worries about transitioning to secondary school? 
+library(tidyverse) 
+library(ggplot2) 
+library(dplyr) 
+library(Hmisc)
+library(psych)
 library(haven) 
-dataset = read_sav("SchoolTransitionData.sav")
-akdjlakjdlkfj 
+library(foreign)
+dataset <- read_sav("SchoolTransitionData.sav") 
+
+dataset <- dataset %>% as.data.frame() %>% select(ID,attain_average, starts_with("C1_SConcern")) %>% filter(!is.na(attain_average) & !is.na(C1_SConcern1))
+
+
+dat <- read.spss("SchoolTransitionData.sav") %>%
+  as.data.frame() %>%
+  select(ID, attain_average, starts_with("C1_SConcern")) %>% # select variables of interest
+  filter(!is.na(attain_average) & !is.na(C1_SConcern1)) %>% # exclude NAs for academic performance 
+  mutate(C1_SConcern1 = ifelse(C1_SConcern1== "Not at all worried", 1, C1_SConcern1)) 
