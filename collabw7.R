0104ed93031ffcff858cd1e7443f366cedb59d54
Cindy Chen <ches53749@gmail.com>
2025-03-13 09:46:43 +0000

    new one for 3/13

--- collabw7.R
+++ collabw7.R
@@ -8,11 +8,9 @@ library(haven)
 library(foreign)
 dataset <- read_sav("SchoolTransitionData.sav") 
 
-dataset <- dataset %>% as.data.frame() %>% select(ID,attain_average, starts_with("C1_SConcern")) %>% filter(!is.na(attain_average) & !is.na(C1_SConcern1))
-
-
-dat <- read.spss("SchoolTransitionData.sav") %>%
+dataset1 <- read_sav("SchoolTransitionData.sav") %>%
   as.data.frame() %>%
-  select(ID, attain_average, starts_with("C1_SConcern")) %>% # select variables of interest
-  filter(!is.na(attain_average) & !is.na(C1_SConcern1)) %>% # exclude NAs for academic performance 
-  mutate(C1_SConcern1 = ifelse(C1_SConcern1== "Not at all worried", 1, C1_SConcern1)) 
+  select(ID, attain_average, starts_with("C1_SConcern")) %>% 
+  filter(!is.na(attain_average) & !is.na(C1_SConcern1)) %>% 
+  mutate(across(C1_SConcern1:C1_SConcern20, 
+                ~ as.numeric(ifelse(as.character(.) == "Not at all worried", 1, as.character(.))))) 
